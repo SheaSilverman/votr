a7910226a48ff71e8d25ae0e4adca4486e36c9ce
Shea Silverman <shea@sheasilverman.com>
2016-05-07 20:39:28 +0000

    change

--- core/views.py
+++ core/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render, HttpResponse
+from django.views.generic import ListView
+from models import Voter
+
+
 
 # Create your views here.
 def index(request):
 	return HttpResponse("Hi")
 
+def voter(request):
+	voters = Voter.objects.all()
+	context = {'voters': voters}
+	return render(request, 'voter_list.html', context)
+
+
+
+# class VoterList(ListView):
+#     model = Voter
--- votr/urls.py
+++ votr/urls.py
@@ -20,4 +20,6 @@ from core import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
+    url(r'^voter/', views.voter, name='voter'),
+
 ]
