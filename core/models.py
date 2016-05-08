18269b3f0f95328665ec89464d9f261cf1ce0f21
shea@sheasilverman.com <shea@sheasilverman.com>
2016-05-08 02:15:35 +0000

    import csv

--- core/models.py
+++ core/models.py
@@ -6,25 +6,60 @@ from django.db import models
 class Party(models.Model):
 	name = models.CharField(max_length=200)
 
+
+	#Gender	Race	DOB	Registration	Party	Precinct	Group	Split	Suffix	Status	Congress	House	Senate	Commision	SchoolBoard	AreaCode	PhoneNumber	Extension	email	latitude	longitude
+
 class Voter(models.Model):
+	county = models.CharField(max_length=200)
+	voterID = models.IntegerField(default=0)
+
+
 	first_name = models.CharField(max_length=200)
 	last_name = models.CharField(max_length=200)
-	street1 = models.CharField(max_length=200)
-	street2 = models.CharField(max_length=200)
+	middle_name = models.CharField(max_length=200)
+	suffix = models.CharField(max_length=200)
+	exempt = models.CharField(max_length=200)
+
+
+	address1 = models.CharField(max_length=200)
+	address2 = models.CharField(max_length=200)
 	city = models.CharField(max_length=200)
 	state = models.CharField(max_length=200)
 	zipcode = models.CharField(max_length=200)
+
+	mailing_address1 = models.CharField(max_length=200)
+	mailing_address2 = models.CharField(max_length=200)
+	mailing_address2 = models.CharField(max_length=200)
+	mailing_city = models.CharField(max_length=200)
+	mailing_state = models.CharField(max_length=200)
+	mailing_zipcode = models.CharField(max_length=200)
+	mailing_country = models.CharField(max_length=200)
+
+	gender = models.CharField(max_length=200)
+	race = models.CharField(max_length=200)
+	dob = models.CharField(max_length=200)
+	registration = models.CharField(max_length=200)
+	party = models.CharField(max_length=5)
+	
+	precinct = models.IntegerField(default=0)
+	group = models.CharField(max_length=200)
+	split = models.CharField(max_length=200)
+	extra_suffix = models.CharField(max_length=200)
+	status = models.CharField(max_length=200)
+	
 	state_house = models.IntegerField(default=0)
 	state_senate = models.IntegerField(default=0)
-	us_house = models.IntegerField(default=0)
-	us_senate = models.IntegerField(default=0)
+	congress = models.IntegerField(default=0)
 	school_board= models.IntegerField(default=0)
-	active = models.IntegerField(default=0)
+
 	email = models.CharField(max_length=200)
 	phone = models.CharField(max_length=200)
-	party = models.ForeignKey(Party)
+	#party = models.ForeignKey(Party)
 	latitude = models.CharField(max_length=200)
 	longitude = models.CharField(max_length=200)
+
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
 
 
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from models import Voter
 
@@ -6,14 +7,27 @@ from models import Voter
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hi")
+    return HttpResponse("Hi")
 
 def voter(request):
-	voters = Voter.objects.all()
-	context = {'voters': voters}
-	return render(request, 'voter_list.html', context)
+    voter_list = Voter.objects.all()
+    #context = {'voters': voters}
+    #return render(request, 'voter_list.html', context)
 
 
+    paginator = Paginator(voter_list, 25) # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        voters = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        voters = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        voters = paginator.page(paginator.num_pages)
+
+    return render(request, 'voter_list.html', {'voters': voters})
 
 # class VoterList(ListView):
 #     model = Voter
