3c7a3da20ee4d38bb555c875ef50f5aca187033b
Shea <shea@sheasilverman.com>
2018-09-05 14:20:36 -0400

    Added new geocoder

--- core/management/commands/fl_geocode.py
+++ core/management/commands/fl_geocode.py
@@ -2,7 +2,9 @@ from django.core.management.base import BaseCommand, CommandError
 from core.models import Voter, VoterException, VoterLatLongException
 import requests
 
-ngrok_url = "http://454fa8e4.ngrok.io/v1/search?text="
+from multiprocessing.pool import ThreadPool
+
+ngrok_url = "http://127.0.0.1/v1/search?text="
 
 class Command(BaseCommand):
     help = 'Adds Lat/Long to VoterFile'
@@ -10,30 +12,26 @@ class Command(BaseCommand):
     # def add_arguments(self, parser):
     #     parser.add_argument('csv_path', nargs='+', type=str)
 
-
     def handle(self, *args, **options):
-        count = Voter.objects.all().count()
-        print count
+        pool = ThreadPool(8)
+        pool.map(get_voter, xrange(Voter.objects.all().count()))
 
-        # count = 10
-        # print count
-        i = 1
-        while i < count:
-            print(i)
-            try:
-                v = Voter.objects.get(pk=i)
-                url = "%s%s %s %s %s %s" % (ngrok_url, v.address1, v.address2, v.city, v.state, v.zipcode)
-                print(url)
-                r = requests.get(url)
-                parsed_json = r.json()
-                longitude = parsed_json['features'][0]['geometry']['coordinates'][0]
-                latitude = parsed_json['features'][0]['geometry']['coordinates'][1]
-                v.longitude = longitude
-                v.latitude = latitude
-                v.save()
-            except:
-                print "Exception Occured"
-                print i
-                import sys, traceback
-                print traceback.print_exc()
-            i += 1
+ 
+def get_voter(id):
+    print id
+    try:
+        v = Voter.objects.get(pk=id)
+        url = "%s%s %s %s %s %s" % (ngrok_url, v.address1, v.address2, v.city, v.state, v.zipcode)
+        r = requests.get(url)
+        parsed_json = r.json()
+        longitude = parsed_json['features'][0]['geometry']['coordinates'][0]
+        latitude = parsed_json['features'][0]['geometry']['coordinates'][1]
+        v.longitude = longitude
+        v.latitude = latitude
+        v.save()
+    except:
+        print "Exception Occured"
+        print id
+        import sys, traceback
+        print traceback.print_exc()
+     
---
+++ core/management/commands/fl_new_geocoder.py
@@ -0,0 +1,72 @@
+from django.core.management.base import BaseCommand, CommandError
+from core.models import Voter, VoterException
+import csv
+import requests
+
+ngrok_url = "http://127.0.0.1/v1/search?text="
+
+
+class Command(BaseCommand):
+    help = 'Imports Voter Information CSV'
+
+    def add_arguments(self, parser):
+        parser.add_argument('csv_path', nargs='+', type=str)
+
+
+    def handle(self, *args, **options):
+        for csv_path in options['csv_path']:
+            line = 0
+            for row in get_csv_row(csv_path):
+                if row[0] != 'address1': # Ignore the header row, import everything else
+                    try:
+                        #addr = row[0] + " " + row[1] + " " + row[2] + " " + row[3] + " " + row[4]
+                        address1 = row[0]
+                        address2 = row[1]
+                        city = row[2]
+                        state = row[3]
+                        zipcode = row[4]
+                        latitude, longitude = get_latlong(address1, address2, city, state, zipcode)
+
+                        voters = Voter.objects.filter(address1=address1, address2=address2, city=city, state=state,zipcode=zipcode)
+                        for v in voters:
+                            v.longitude = longitude
+                            v.latitude = latitude
+                            v.save()
+
+                    except:
+                        print "Exception Occured"
+                        print id
+                        import sys, traceback
+                        print traceback.print_exc()
+
+                    # voter.latitude = row[38]
+                    # voter.longitude = row[39]
+                    #voters.append(voter)
+                    #voter.save()
+                    
+                    line +=1
+                    print line
+
+
+def get_latlong(address1, address2, city, state, zipcode):
+    print address1
+    try:
+        url = "%s%s %s %s %s %s" % (ngrok_url, address1, address2, city, state, zipcode)
+        r = requests.get(url)
+        parsed_json = r.json()
+        longitude = parsed_json['features'][0]['geometry']['coordinates'][0]
+        latitude = parsed_json['features'][0]['geometry']['coordinates'][1]
+        return longitude, latitude
+    except:
+        print "Exception Occured"
+        print id
+        import sys, traceback
+        print traceback.print_exc()
+
+def get_csv_row(filename):
+    #SELECT distinct address1, address2, city, state, zipcode
+    #  FROM core_voter;
+    with open(filename, "rb") as csvfile:
+        datareader = csv.reader(csvfile, delimiter=';', quotechar='"')
+        for row in datareader:
+            yield row
